77b9be5b541e9a65b396a352e50830c5ec901985
ChuanGitIt <zhaochuansailout@gmail.com>
2024-11-24 23:15:07 +0000

    fixed part1

--- GroupProject/ProbablyADirectory/Part1.R
+++ GroupProject/ProbablyADirectory/Part1.R
@@ -1,28 +1,24 @@
 
 num_P <- 1000 #number of P values calculated
 experiment_size <- 1000 #number of simulations for each calculation of P
-deltaT <- 0.01  #small time increment; also variance
+deltaT <- 0.1  #small time increment; also variance
 numSteps <- 1/deltaT #number of random increments
-p_values <- numeric(num_P)    #create an array of P value
+p_values <- numeric(experiment_size)    #create an array of P value
 
-for(i in 1:num_P){       #outer loop : gather P values
 
-successCount <- 0
-
-for(j in 1 : experiment_size){     #inner loop : compute P
+for(j in 1 : experiment_size){     #compute P
   cumulationSum <- 0 #sum of Temperature
-  
-  deltaY1 <- rnorm(numSteps,mean=0,sd=sqrt(deltaT))   # random increments(follow normal distribution)
-  cumulationSum <- sum(deltaY1)  #sum of the increments
-
-if(cumulationSum>0)                 #count the successful experiments
-{
-  successCount <- successCount + 1      
-}
-  
-}
-
-p_values[i] <- successCount/experiment_size   #probabilty of above zero, stored in p_values
+  successCount <- 0  #above zero time
+  print(j/experiment_size) # progress marker
+  for(i in 1 : numSteps){
+  deltaY1 <- rnorm(1,mean=0,sd=sqrt(deltaT))   # random increments(follow normal distribution)
+  cumulationSum <- cumulationSum + deltaY1  #sum of the increments
+    if(cumulationSum>0)                 #count successful time period
+    {
+      successCount <- successCount + 1      
+    }
+  }
+  p_values[j] <- successCount/numSteps   #probabilty of above zero, stored in p_values
 }
 
 hist(p_values, breaks = 50, probability = TRUE, col = "orange",main="Distribution of P",xlab ="P",
@@ -30,11 +26,13 @@ hist(p_values, breaks = 50, probability = TRUE, col = "orange",main="Distributio
 
 
 num_T <- 1000 #number of Tmax calculated
-deltaT <- 0.01  #small time increment; also variance
+deltaT <- 0.00001  #small time increment; also variance
 numSteps <- 1/deltaT #number of random increments
 maxT <- numeric(num_T)   # array of Tmax
 
 for(i in 1:num_T){   #calculate all the Tmaxs
+  if(((i/num_T)*100)%%10 ==0)
+    print(i/num_T)
 curr_Tmax <- 0
 curr_T <- 0
   for(j in 1:numSteps){    #calculate one Tmax
